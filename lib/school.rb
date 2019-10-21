de591f8c755b74e578fa5062a550d6ad8e0c1272
Javi <javsteratii@gmail.com>
2019-10-20 21:05:06 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+    attr_accessor :roster, :student_name, :school_name
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new{ |h, k| h[k] = [] }
+  end
+  
+  
+  def add_student(student_name, grade)
+      @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+     h.each_value do |key|
+       puts value  
+       end 
